f9f456286b0a837e61f530a3bc8116aba4f01563
Hyeyeon-Ko <142862000+Hyeyeon-Ko@users.noreply.github.com>
2023-08-23 21:24:18 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,10 +1,5 @@
 n = int(input())
 n_list = list(map(int, input().split()))
 p = int(input())
-count = 0
 
-for i in range(n):
-    if n_list[i] == p:
-        count += 1
-    
-print(count)
+print(n_list.count(p))
