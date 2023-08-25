f8a96978b14f29598965af679326fbea68b81225
Hyeyeon-Ko <142862000+Hyeyeon-Ko@users.noreply.github.com>
2023-08-25 11:10:33 +0900

    [Bronze I] Title: 단어 공부, Time: 120 ms, Memory: 33212 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,13 @@
+word = str(input()).upper()
+word_list = list(set(word))
+cnt = []
+
+for a in word_list:
+    count = word.count(a)
+    cnt.append(count)
+
+if cnt.count(max(cnt)) >= 2:
+    print("?")
+
+else:
+    print(word_list[(cnt.index(max(cnt)))])
