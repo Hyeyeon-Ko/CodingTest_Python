dca0986c35e1c12250827c521b0de30b409839c7
Hyeyeon-Ko <142862000+Hyeyeon-Ko@users.noreply.github.com>
2023-08-25 16:14:01 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,18 @@
+grade_list = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
+score_list = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0]
+total_score = 0
+total_point = 0
+
+for _ in range(20):
+    name, point, grade = input().split()
+    total_point += float(point)
+
+    if grade == 'P':
+        total_point -= float(point)
+    else:
+        for i in grade_list:
+            if i == grade:
+                total_score += float(point) * score_list[grade_list.index(i)]
+                break
+
+print(total_score / total_point)
