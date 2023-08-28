9649a65c6268874e70d123446aa515d3e8d95b60
Hyeyeon-Ko <142862000+Hyeyeon-Ko@users.noreply.github.com>
2023-08-28 22:55:24 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,37 +1,12 @@
+changes = [25, 10, 5, 1]
 T = int(input())
-Q = 25
-D = 10
-N = 5
-P = 1
 
 for _ in range(T):
     C = int(input())
     count = []
 
-    if C // Q > 0:
-        count.append(C // Q)
-        C = C % Q
-    else:
-        count.append(0)
-        
-    if C // D > 0:
-        count.append(C // D)
-        C = C % D
-    else:
-        count.append(0)
+    for i in changes:
+        count.append(C // i)
+        C = C % i
 
-    if C // N > 0:
-        count.append(C // N)
-        C = C % N
-    else:
-        count.append(0)
-
-    if C // P > 0:
-        count.append(C // P)
-    else:
-        count.append(0)
-    
-    for i in count:
-        print(i, end=" ")
-
-    print()
+    print(*count)
