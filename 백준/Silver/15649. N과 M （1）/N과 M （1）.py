9e1133bc6f44a9e2a32f77126bfa6f59c392e254
Hyeyeon-Ko <142862000+Hyeyeon-Ko@users.noreply.github.com>
2024-01-09 11:53:09 +0900

    [Silver III] Title: N과 M (1), Time: 144 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,11 +1,23 @@
-import itertools
+import sys
+sys.setrecursionlimit(10 ** 6) # 재귀 사용시 필수! 재귀 깊이 제한 늘려서 런타임에러 없애기 위함.
 
-n, m = map(int, input().split())
-nums = [i for i in range(1, n+1)]
+input = sys.stdin.readline
 
-array = itertools.permutations(nums, m)
+N, M = map(int, input().split())
+result = []
+visited = [False] * (N+1) # 중복 허용 x -> 방문했는지 체크하는 리스트 필요
 
-for i in array:
-    for j in i:
-        print(j, end = ' ')
-    print()
+def backtracking(num):
+    if num == M:
+        print(' '.join(map(str, result)))
+        return
+    for i in range(1, N+1):
+        if not visited[i]:
+            visited[i] = True
+            result.append(i)
+            backtracking(num + 1)
+
+            visited[i] = False
+            result.pop()
+
+backtracking(0)
