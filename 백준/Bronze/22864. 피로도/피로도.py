42de0a7665dda3563098ba08258e1fa0c17079df
Hyeyeon-Ko <142862000+Hyeyeon-Ko@users.noreply.github.com>
2024-03-18 01:33:38 +0900

    [Bronze II] Title: 피로도, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/22864. 피로도/피로도.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+a, b, c, m = map(int, input().split())
+t = 0 
+w = 0
+
+for i in range(24):
+    if t > m:
+        print(0)
+    else:
+        if t + a <= m:
+            t += a
+            w += b
+        else:
+            if t - c >= 0:
+                t -= c
+            else:
+                t = 0
+print(w)
